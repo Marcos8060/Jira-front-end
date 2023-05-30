0b05b7f6407d194db82c54d0569dad71f8bffc81
MarcosDMT <marcosgav80@gmail.com>
2023-05-30 23:16:34 +0300

    made minor changes

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,22 +4,24 @@ import Link from "next/link";
 
 const Sidebar = () => {
   return (
-    <section className="p-4 space-y-4 border-r border-blue h-[90vh]">
-      <div className="flex items-center justify-between">
-        <h1 className="text-xl font-semibold">Projects</h1>
-        <GrAdd className="bg-blue p-3 text-4xl rounded-full" />
-      </div>
-      <div>
-        <h2 className="uppercase text-sm">Recent</h2>
-      </div>
-      <div>
-        <Link href="/projects">
-          <h2 className="text-sm hover:bg-blue transition duration-300 ease-in-out p-2 rounded cursor-pointer">
-            View All Projects
-          </h2>
-        </Link>
-      </div>
-    </section>
+    <>
+      <section className="p-4 space-y-4 border-r border-blue h-[90vh]">
+        <div className="flex items-center justify-between">
+          <h1 className="text-xl font-semibold">Projects</h1>
+          <GrAdd className="bg-blue p-3 text-4xl rounded-full cursor-pointer" />
+        </div>
+        <div>
+          <h2 className="uppercase text-sm">Recent</h2>
+        </div>
+        <div>
+          <Link href="/projects">
+            <h2 className="text-sm hover:bg-blue transition duration-300 ease-in-out p-2 rounded cursor-pointer">
+              View All Projects
+            </h2>
+          </Link>
+        </div>
+      </section>
+    </>
   );
 };
 
