c7acd2ddc846049df1b2632d75f567f13d66f878
MarcosDMT <marcosgav80@gmail.com>
2023-05-31 00:30:07 +0300

    rendered dynamic content based on active tab index

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,13 @@
 import React, { useState } from "react";
 import { SiJirasoftware } from "react-icons/si";
+import Summary from "./Summary";
+import Board from "./Board";
+import List from "./List";
+import Calender from "./Calender";
+import Timeline from "./Timeline";
+import Forms from "./Forms";
+import Pages from "./Pages";
+import ProjectSettings from "./ProjectSettings";
 
 const tabItems = [
   { name: "Summary" },
@@ -25,8 +33,33 @@ const Main = () => {
 
         <section className="flex items-center gap-8 border-b-2 border-blue">
           {tabItems.map((tab, index) => (
-            <div onClick={()=> setActiveTab(tab.name)} className={tab.name === activeTab ? 'border-b-2 border-bluish' : '' }>
-              <p key={index} className="text-sm my-2 cursor-pointer text-bluish font-semibold">{tab.name}</p>
+            <div
+              onClick={() => setActiveTab(tab.name)}
+              className={
+                tab.name === activeTab ? "border-b-2 border-bluish" : ""
+              }
+            >
+              <p
+                key={index}
+                className="text-sm my-2 cursor-pointer text-bluish font-semibold"
+              >
+                {tab.name}
+              </p>
+            </div>
+          ))}
+        </section>
+
+        <section>
+          {tabItems.map((tab, index) => (
+            <div key={index} className={tab.name === activeTab ? 'block' : 'hidden'}>
+              {tab.name === "Summary" && <Summary />}
+              {tab.name === "Board" && <Board />}
+              {tab.name === "List" && <List />}
+              {tab.name === "Calender" && <Calender />}
+              {tab.name === "Timeline" && <Timeline />}
+              {tab.name === "Forms" && <Forms />}
+              {tab.name === "Pages" && <Pages />}
+              {tab.name === "Project Settings" && <ProjectSettings />}
             </div>
           ))}
         </section>
