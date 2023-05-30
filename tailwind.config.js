c93b558c483a5742350de2b2226fbac0a3baf64d
MarcosDMT <marcosgav80@gmail.com>
2023-05-30 22:19:02 +0300

    installed react icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,13 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors:{
+        primary: '#DEEBFF',
+        yellow: '#FFC400',
+        secondary: '#F4F5F7',
+        white: '#FFFFFF',
+        blue: '#D5E3F8'
+      }
     },
   },
   plugins: [],
