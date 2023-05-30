1b0aa9c10ef8003186d6b5567dca3e994ee0fb4f
MarcosDMT <marcosgav80@gmail.com>
2023-05-30 23:40:27 +0300

    made minor changes

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React,{ useState} from "react";
+import { SiJirasoftware } from "react-icons/si";
+
+const tabItems = [
+    { name: 'Summary'},
+    { name: 'Board'},
+    { name: 'List'},
+    { name: 'Calender'},
+    { name: 'Timeline'},
+    { name: 'Forms'},
+    { name: 'Pages'},
+    { name: 'Project Settings'},
+]
 
 const Main = () => {
-  return (
-    <div>Main</div>
-  )
-}
+    const [currentTab,setCurrentTab] = useState(0)
 
-export default Main
+  return (
+    <>
+      <section className="p-4">
+        <div className="flex items-center gap-4">
+          <SiJirasoftware className="bg-yellow rounded p-2 text-4xl"/>
+          <h1 className="font-semibold text-xl">Jira Clone</h1>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Main;
